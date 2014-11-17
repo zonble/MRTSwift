a989e2f06155f70b62c55fc2b1c3407d059ef222
zonble <zonble@gmail.com>
2014-11-18 00:57:58 +0800

    Updates database.

---
+++ Tools/get_mrt_data.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import urllib
+import urllib2
+import re
+
+def get_options():
+	url = 'http://web.trtc.com.tw/c/TicketALLresult.asp'
+	req = urllib2.Request(url)
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	target = ""
+	for line in the_page.split("\n"):
+		if "optgroup" in line:
+			target = line
+			break
+	pattern = u"value='(.*?)'"
+	a = re.findall(pattern, target)
+	return set(a)
+
+def get(station):
+	url = "http://web.trtc.com.tw/c/TicketALLresult.asp"
+	data = urllib.urlencode({'s2elect': station, 'submit': '確定'})
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	target = ""
+	for line in the_page.split("\n"):
+		if "<tr><td align='center' width='20%'>" in line:
+			target = line
+			break
+	target = target.strip()
+	target = target.split("</tr>")
+	rows = []
+	for s in target:
+		s = s.decode('utf8')
+		pattern = u"<font(.*?)>(.*?)</font>"
+		a = re.findall(pattern, s)
+		if len(a):
+			row = [x[1].strip() for x in a if x[1] != u'→']
+			rows.append(row)
+	return rows
+
+def main():
+	print "get options"
+	sites = get_options()
+	print set(sites)
+	print "get content"
+	all_rows = []
+	for site in sites:
+		try:
+			print site
+			rows = get(site)
+			all_rows.extend(rows)
+		except Exception as e:
+			print e
+
+	with open("price.txt", "w") as f:
+		txt = ""
+		for row in all_rows:
+			txt += ",".join(row) + "\n"
+		f.write(txt.encode("utf-8"))
+
+	with open("price.sql", "w") as f:
+		txt = "create table data (from_station, to_station, oneway, easycard, reduced_fare, time);\n"
+		for row in all_rows:
+			row[0] = "\"%s\"" % row[0]
+			row[1] = "\"%s\"" % row[1]
+			txt += "insert into data values(" + ",".join(row) + ");\n"
+		txt += "create index from_station_index on data (from_station);\n"
+		txt += "create index to_station_index on data (to_station);\n"
+		f.write(txt.encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
