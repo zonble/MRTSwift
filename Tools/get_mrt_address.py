21a15d68d92659483c982ead77140eaea9a50fd8
zonble <zonble@gmail.com>
2017-12-27 14:06:33 +0800

    Updates the tool to fetch addresses.

--- Tools/get_mrt_address.py
+++ Tools/get_mrt_address.py
@@ -6,7 +6,8 @@ import urllib2
 import re
 
 def get_options():
-	url = 'http://web.trtc.com.tw/c/TicketALLresult.asp'
+	# url = 'http://web.trtc.com.tw/c/TicketALLresult.asp'
+	url = 'http://www.metro.taipei/ct.asp?xItem=78479152&CtNode=70089&mp=122035'
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
 	the_page = response.read()
@@ -15,27 +16,36 @@ def get_options():
 		if "optgroup" in line:
 			target = line
 			break
-	pattern = u"value='(.*?)'"
+	# print target
+	pattern = u"value=\"(.*?)\""
 	a = re.findall(pattern, target)
 	return set(a)
 
 def get(station):
-	url = "http://web.trtc.com.tw/c/stationdetail2010.asp?ID=" + station
+	url = "http://web.metro.taipei/c/stationdetail2010.asp?ID=" + station
+	print "=" * 40
+	print url
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
 	the_page = response.read()
-	pattern = r'<TD BGCOLOR="#ffffff" CLASS="Default"><font size="-1">(.*?)</font></TD>'
+	# pattern = r'<TD BGCOLOR="#ffffff" CLASS="Default"><font size="-1">(.*?)</font></TD>'
+	pattern = r'<div style="float: left;margin: 10px 0 0 3px;font-size: 18px; font-weight: bold;letter-spacing: 0.05em">(.*?)</div>'
 	a = re.findall(pattern, the_page)
+	print a[0]
 	name = a[0]
 	pattern = r"<TD valign='center' BGCOLOR=\"#ffffff\" CLASS=\"Default\"><font size=\"-1\">(.*?)</font>&nbsp;"
 	a = re.findall(pattern, the_page)
+	print a[0]
 	address = a[0]
 	pattern = r"'googlemap.asp\?Longitude=(.*?)&Latitude=(.*?)'"
 	a = re.findall(pattern, the_page)
+	print  a[0][0], a[0][1]
 	return (name, address, a[0][0], a[0][1])
 
 def main():
 	sites = get_options()
+	# print sites
+	# return
 	address = []
 	for site in sites:
 		result = get(site)
