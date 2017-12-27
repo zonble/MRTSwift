0131cfd433f96e4f63b2b58f05ede3402e072c2a
zonble <zonble@gmail.com>
2017-12-28 01:30:22 +0800

    Adds a target for Apple TV.

---
+++ MRTLibTV/MRTLibTV.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRTLibTV.
+FOUNDATION_EXPORT double MRTLibTVVersionNumber;
+
+//! Project version string for MRTLibTV.
+FOUNDATION_EXPORT const unsigned char MRTLibTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRTLibTV/PublicHeader.h>
+
+#import "sqlite3.h"
+#import <time.h>
+
